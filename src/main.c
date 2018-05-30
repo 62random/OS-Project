094147bc26479f709cd8968b8471ea80bb56388b
Megamind51 <alexrone.rone3@gmail.com>
2018-05-30 14:42:21 +0100

    save

--- src/main.c
+++ src/main.c
@@ -11,6 +11,7 @@
 
 #define TAM_STR 50
 #define TAM_STR_MAIN 100
+#define PATCH_TMP "/tmp/aux"
 
 
 
@@ -26,7 +27,6 @@ void executa(LCMD comando, int aux_2){
 	char buffer;
 
 	int c = 0;
-
 	for(aux = comando; aux; aux = aux->prox )
 		i++;
 
@@ -50,7 +50,7 @@ void executa(LCMD comando, int aux_2){
             dup2(p[0],0);
 			close(p[0]);close(p[1]);
 
-			sprintf(str_aux,"aux_%d_%d",aux_2,c);
+			sprintf(str_aux,"%s/aux_%d_%d",PATCH_TMP,aux_2,c);
 			fd1 = open(str_aux , O_WRONLY | O_TRUNC | O_CREAT , 00644);
 			while ( (n = read(0,&buffer,1)) > 0)
     			write(fd1,&buffer,n);
@@ -76,6 +76,10 @@ int main(int argc, char const *argv[]) {
 		exit(-1);
 	}
 
+    mkdir(PATCH_TMP, 0777	);
+
+
+
 	int fd = open(argv[1], O_RDWR | O_CREAT, 00644);
 
 	LCMD aux = parser(fd);
@@ -93,9 +97,12 @@ int main(int argc, char const *argv[]) {
 	for(int d = 0; d < r; d++){
 		wait(NULL);
 	}
+	// fazer output
 
-
-
-
+	if (!fork()){
+		execlp("rm","rm","-r","/tmp/aux",NULL);
+		perror("Fallha ao dar delete");
+		_exit(-1);
+	}
 	return 1;
 }

8fafe4baaa1ce3a13d2aa8c98c1b8936e8e6d5a3
62random <josepedropinto98@gmail.com>
2018-06-02 23:55:23 +0100

    final

--- src/script.sh
+++ src/script.sh
@@ -5,17 +5,23 @@ read n
 #construir ficheiros automaticamente (definidos aqui)
 if [[ $n == 1 ]]; then
 	#Primeira linha de cada ficheiro
-	echo "note1.nb" > note1.nb
-	echo "note2.nb" > note2.nb
+	echo "ex1.nb" > ex1.nb
+	echo "ex2.nb" > ex2.nb
+
 
 
 	#Seguintes linhas de cada ficheiro
-	echo "$ ls -l" >> note1.nb
-	echo "$| wc" >> note1.nb
+	echo "$ ls -l" >> ex1.nb
+	echo "$| wc" >> ex1.nb
 
-
-	echo "$ man man" >> note2.nb
-	echo "$| wc" >> note2.nb
+	echo "lista ps" >> ex2.nb
+	echo "$   ps" >> ex2.nb
+	echo "a segunda cena" >> ex2.nb
+	echo "$|  head -2" >> ex2.nb
+	echo "wordcount é fixe" >> ex2.nb
+	echo "$1| wc" >> ex2.nb
+	echo "grep a pid" >> ex2.nb
+	echo "$3| grep PID" >> ex2.nb
 fi
 
 #construir ficheiro linha após linha
